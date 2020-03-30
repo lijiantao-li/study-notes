00ba18ec4af7c811991238118d0dd14b0314d9b6
li <948660943@qq.com>
2020-03-30 13:13:53 +0800

    second version for dos

--- 批处理/修改文件名称.bat
+++ 批处理/修改文件名称.bat
@@ -1,14 +1,15 @@
 chcp 65001
+setlocal enabledelayedexpansion
 set subject="学科"
 set chapter="第一章"
-set type="cpp"
 set separator="-"
 set /a yes=0
 set /a no=0
 echo 完成情况>result.txt
-for /f "tokens=1,2 delims=	" %%i in (学号表.txt) do IF EXIST *%%i* (ren *%%i* %%i%separator%%%j%separator%%subject%%separator%%chapter%.%type%
+for /f "tokens=1,2 delims=	" %%i in (学号表.txt) do IF EXIST *%%i* ( for /f "tokens=2 delims=." %%x in ('dir /b *%%i*') do set type=%%x
+ren *%%i* %%i%separator%%%j%separator%%subject%%separator%%chapter%.!type!
 echo %%i 	完成>>result.txt
-set /a yes+=1 ) ELSE ( echo %%i 	未完成>>result.txt
+set /a yes+=1) ELSE ( echo %%i 	未完成>>result.txt
 set /a no+=1 ) 
 echo 完成人数：%yes%>>result.txt
 echo 未完成人数：%no%>>result.txt
